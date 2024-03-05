c93578abd18b714e7248d05b15546aa8a569d997
TE22B-Ullenius-Bion <bion.holmbergullenius@elev.ga.ntig.se>
2024-03-05 22:51:19 +0100

    färdig

--- scrip.js
+++ scrip.js
@@ -1,12 +1,19 @@
+var varukorg = JSON.parse(localStorage.getItem("varukorg")) || [];
 
-
-
-
-function add(namn, pris)
-{
-    let obj = {
-        variabel:värd1,
-        variabel2: värde2
-    }
-    console.log("Add anropad");
+function add(produkt,pris) {
+    varukorg.push({namn: produkt, pris: pris})
+    localStorage.setItem("varukorg", JSON.stringify(varukorg));
 }
+
+var container = document.createElement("div");
+
+varukorg.forEach(element => {
+    var namn = element.namn;
+    var pris = element.pris;
+
+    var text = document.createElement("p");
+    text.innerHTML = "Produkt: " + namn + ", Pris: " + pris;
+    container.appendChild(text);
+});
+
+document.body.appendChild(container);
